25fdf4d37518972e3aa7c818692e2bbce1cd12ac
Shaon2225 <nazim1703073@stud.kuet.ac.bd>
2022-05-13 19:55:04 +0600

    varified email done

--- src/RequreAuth.jsx
+++ src/RequreAuth.jsx
@@ -2,15 +2,23 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
 import auth from "./Firebase.init";
 
-function RequireAuth({ children}) {
-    const [user]=useAuthState(auth);
-    let location = useLocation();
-  
-    if (!user) {
-      return <Navigate to="/login" state={{ from: location }} replace />;
-    }
-  
+function RequireAuth({ children }) {
+  const [user] = useAuthState(auth);
+  let location = useLocation();
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  if (!user?.emailVerified) {
+    return (
+      <div className="login-container">
+        <h1 className="login-title">Please varify your email</h1>
+        <button className="submit-btn px-3">Send varify email</button>
+      </div>
+    );
+  }
+  else{
     return children;
   }
+}
 
 export default RequireAuth;
